3816da71601d8a07ce382906c31584be5da4bc30
Brashan <brashanm@gmail.com>
2023-11-09 14:43:52 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Brashan Mohanakumar on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
